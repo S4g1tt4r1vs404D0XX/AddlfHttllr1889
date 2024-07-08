b1cbc1c1c2e4fd372f336ce60f47d212f5213cc6
S4g1tt4r1vs404D0XX <143964142+S4g1tt4r1vs404D0XX@users.noreply.github.com>
2024-07-08 05:34:54 +0300

    Update wormcord.py

--- wormcord.py
+++ wormcord.py
@@ -1,1 +1,17 @@
+from flask import Flask, jsonify, request
 
+app = Flask(__name__)
+
+users = {}
+guilds = {}
+channels = {}
+messages = {}
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    user = users.get(user_id)
+    if user:
+        return jsonify(user)
+    return jsonify({'error': 'User not found'}), 404
+
+# Uncontinued script, finishing tomorrow!
