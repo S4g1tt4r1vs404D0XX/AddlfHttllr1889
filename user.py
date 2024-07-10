deb9760e6b3d4f4f8f2b81dbcf6aefa2fa02d0d9
S4g1tt4r1vs404D0XX <143964142+S4g1tt4r1vs404D0XX@users.noreply.github.com>
2024-07-10 16:40:10 +0300

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, id, username, avatar):
+    def __init__(self, id, username, avatar, ip):
         self.id = id
         self.username = username
         self.avatar = avatar
+        self.ip = ip
