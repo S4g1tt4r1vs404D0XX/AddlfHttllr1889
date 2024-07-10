5f2d97491e55b266cd899278f7ceb6e1e400c3e9
S4g1tt4r1vs404D0XX <143964142+S4g1tt4r1vs404D0XX@users.noreply.github.com>
2024-07-10 05:24:29 +0300

    Create message.py

---
+++ message.py
@@ -0,0 +1,6 @@
+class Message:
+    def __init__(self, id, channel_id, author, content):
+        self.id = id
+        self.channel_id = channel_id
+        self.author = author
+        self.content = content
