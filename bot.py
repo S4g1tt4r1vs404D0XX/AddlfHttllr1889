cadd3840fb3c515464d0926d0b39575855b15ad5
S4g1tt4r1vs404D0XX <143964142+S4g1tt4r1vs404D0XX@users.noreply.github.com>
2024-07-12 02:20:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,44 @@
-import asyncio
 import aiohttp
+import asyncio
 import json
-import websockets
 from events import events
 
 DISCORD_GATEWAY_URL = "wss://gateway.discord.gg/?v=10&encoding=json"
 
 class Bot:
-    def __init__(self, api_url):
+    def __init__(self, api_url, command_prefix='!'):
         self.api_url = api_url
         self.events = events()
         self.token = None
         self.session = None
         self.ws = None
+        self.prefix = command_prefix
+        self.commands = {}
 
     def event(self, func):
         return self.events.event(func)
 
+    def command(self, name=None):
+        def decorator(func):
+            self.commands[name or func.__name__] = func
+            return func
+        return decorator
+
     async def start(self, token):
         self.token = token
         self.session = aiohttp.ClientSession()
-        await self.events.emit('on_ready')
         await self.connect_to_discord_gateway()
 
     async def close(self):
         await self.session.close()
 
     async def connect_to_discord_gateway(self):
-        async with websockets.connect(DISCORD_GATEWAY_URL) as ws:
-            self.ws = ws
-            await self.identify()
-            async for message in ws:
-                await self.handle_gateway_message(message)
+        async with aiohttp.ClientSession() as session:
+            async with session.ws_connect(DISCORD_GATEWAY_URL) as ws:
+                self.ws = ws
+                await self.identify()
+                async for message in ws:
+                    await self.handle_gateway_message(message)
 
     async def identify(self):
         payload = {
@@ -46,7 +53,7 @@ class Bot:
                 }
             }
         }
-        await self.ws.send(json.dumps(payload))
+        await self.ws.send_json(payload)
 
     async def handle_gateway_message(self, message):
         message = json.loads(message)
@@ -54,10 +61,18 @@ class Bot:
             await self.events.emit('on_ready')
         elif message['t'] == 'MESSAGE_CREATE':
             await self.events.emit('on_message', message['d'])
+            await self.handle_message_create(message['d'])
         elif message['t'] == 'GUILD_MEMBER_ADD':
             await self.events.emit('on_member_join', message['d'])
         elif message['t'] == 'GUILD_MEMBER_REMOVE':
             await self.events.emit('on_member_leave', message['d'])
+
+    async def handle_message_create(self, message):
+        content = message['content']
+        if content.startswith(self.prefix):
+            cmd_name, *args = content[len(self.prefix):].split()
+            if cmd_name in self.commands:
+                await self.commands[cmd_name](self, message, *args)
 
     def run(self, token):
         loop = asyncio.get_event_loop()
@@ -69,10 +84,10 @@ class Bot:
             loop.close()
 
     async def get_user(self, user_id):
-        response = await self.session.get(f"{self.api_url}/api/users/{user_id}", headers={"Authorization": f"Bot {self.token}"})
-        user = await response.json()
-        await self.events.emit('on_user_fetched', user)
-        return user
+        async with self.session.get(f"{self.api_url}/users/{user_id}") as response:
+            user = await response.json()
+            await self.events.emit('on_user_fetched', user)
+            return user
 
     async def get_guild(self, guild_id):
         response = await self.session.get(f"{self.api_url}/api/guilds/{guild_id}", headers={"Authorization": f"Bot {self.token}"})
