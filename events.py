af5bb0ad8101f75eb0aa0eef97981bf51300aa85
S4g1tt4r1vs404D0XX <143964142+S4g1tt4r1vs404D0XX@users.noreply.github.com>
2024-07-11 15:18:21 +0300

    Create events.py

---
+++ events.py
@@ -0,0 +1,23 @@
+# event_emitter.py
+import asyncio
+
+class EventEmitter:
+    def __init__(self):
+        self._events = {}
+
+    def event(self, func):
+        self.on(func.__name__, func)
+        return func
+
+    def on(self, event, listener):
+        if event not in self._events:
+            self._events[event] = []
+        self._events[event].append(listener)
+
+    async def emit(self, event, *args, **kwargs):
+        if event in self._events:
+            for listener in self._events[event]:
+                if asyncio.iscoroutinefunction(listener):
+                    await listener(*args, **kwargs)
+                else:
+                    listener(*args, **kwargs)
